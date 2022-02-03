028b6bec5b14e4b9a66c92b2331ddc41dfe7e3c4
tripletens <tripletens.kc@gmail.com>
2022-02-03 11:49:49 +0100

    Initial commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.index')
+@section('content')
+    @include('const.navbar')
+    <p> landing page here </p>
+@endsection
---
+++ resources/views/layouts/index.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> Url Shortener </title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" >
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"></script>
+
+    </head>
+    <body>
+        @yield('content')
+    </body>
+</html>
